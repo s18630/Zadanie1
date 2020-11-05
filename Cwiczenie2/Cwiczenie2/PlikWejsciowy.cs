ed4b44c03b0afe80a83fcd5ce01f8813c91c7f0f
Weronika Jaworek <s18630@pjwstk.edu.pl>
2020-11-06 00:31:36 +0100

    początek

---
+++ Cwiczenie2/Cwiczenie2/PlikWejsciowy.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace Cwiczenie2
+{
+    public class PlikWejsciowy
+    {
+        public string path;
+        public List<string> content;
+        private object ex;
+
+        public PlikWejsciowy(string path)
+        {
+           
+            if (File.Exists(path))
+            {
+                this.path = path;
+            }else
+            {
+                throw new Exception(" Scieżka nei istnieje");
+            }
+
+            content = new List<string>();
+
+            readContent();
+
+        }
+
+
+        public PlikWejsciowy()
+        {
+            path = @"dane.csv";
+         
+            content = new List<string>();
+
+             readContent();
+
+
+
+        }
+
+  
+        private void readContent(){
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    content.Add(s);
+                   
+                }
+            }
+
+
+
+        }
+
+
+        public void showContent()
+        {
+
+         foreach(string s in content)
+         {
+           Console.WriteLine(s);
+         }
+
+        }
+
+
+        public List<string> getContent()
+        {
+            return content;
+        }
+
+
+
+    }
+
+
+
+}
+
+
+
+
+    
+
+    
+    
+
+
+  
+
+
+    
+
---
+++ Cwiczenie2/Cwiczenie2/Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Xml.Schema;
+
+namespace Cwiczenie2
+{
+    class Program
+    {
+     
+
+        public enum FormatDanych
+        {
+            xml,
+            json
+        };
+
+        static void Main(string[] args)
+        { 
+            string AdresPlikuCSV = "data.csv";
+        string adresScieżkiDocelowej = "żesult.xml";
+        FormatDanych formatDanych = FormatDanych.xml;
+            Console.WriteLine(formatDanych);
+
+            /*resPlikuCSV = args[0];
+            adresScieżkiDocelowej = args[1];
+            string xml= Enum.GetName(typeof(FormatDanych), 1);
+            if(xml.Equals(args[3])== true)
+            {
+                formatDanych = FormatDanych.xml;
+            } */
+
+            string curFile = @"c:\temp\test.txt";
+            Console.WriteLine(File.Exists(curFile) ? "File exists." : "File does not exist.");
+            curFile = @"dane.csv";
+            Console.WriteLine(File.Exists(curFile) ? "File exists." : "File does not exist.");
+
+            List<string> students =  new List<string>();
+
+
+            using (StreamReader sr = File.OpenText(curFile))
+            {
+                string s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    students.Add(s);
+                    Console.WriteLine(s);
+                }
+            }
+
+
+            Console.WriteLine("------------------------------------");
+            foreach (string s in students)
+            {
+
+                Console.WriteLine(s);
+             
+
+
+
+            }
+            Console.WriteLine("00000000000000000000000000000000000-");
+            Console.WriteLine("00000000000000000000000000000000000-");
+            Console.WriteLine("00000000000000000000000000000000000-");
+
+            foreach (string s in students)
+            {
+
+                Console.WriteLine(s);
+
+                string[] student = s.Split(',');
+                Console.WriteLine(student[0]);
+                Console.WriteLine(student[1]);
+                Console.WriteLine(student[2]);
+                Console.WriteLine(student[3]);
+
+
+
+
+            };
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("SPRAWDZENIE KLASY");
+            Console.WriteLine();
+            PlikWejsciowy pw = new PlikWejsciowy();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine(pw.path);
+           Console.WriteLine("SPRAWDZENIE KLASYpw.content");
+            Console.WriteLine(pw.content);
+
+
+            Console.WriteLine("SPRAWDZENIE KLASY nie działająca ścieżka ");
+            try
+            {
+                PlikWejsciowy w = new PlikWejsciowy("aaaaaaaaaaaa");
+            } catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+                Console.WriteLine("nie mozńa utworzyc pliku");
+            }
+
+
+            Console.WriteLine("SPRAWDZENIE KLASY  działająca ścieżka ");
+            Console.WriteLine( );
+            try
+            {
+                PlikWejsciowy we= new PlikWejsciowy(curFile);
+
+                //   Console.WriteLine(we.path);
+                //    Console.WriteLine(we.content);
+                we.showContent();
+                List<string> lista = we.getContent();
+                Console.WriteLine("SPRAWDZENIE  metody GET");
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                foreach (string s in lista)
+                {
+                    Console.WriteLine(s);
+                }
+
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+                Console.WriteLine("nie mozńa utworzyc pliku");
+            }
+
+
+          
+
+
+
+
+        }
+
+
+
+
+
+
+    }
+
+    }
+
+
+
+
+
+
+
+
+        
+      
+
+
+
+       
+
+
+
