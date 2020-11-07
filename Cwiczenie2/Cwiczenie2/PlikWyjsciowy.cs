7749111263ec8451032b3f973f3e798972092e42
Weronika Jaworek <s18630@pjwstk.edu.pl>
2020-11-07 01:53:49 +0100

    nasteony krok to stworzyc plik xml

---
+++ Cwiczenie2/Cwiczenie2/PlikWyjsciowy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Cwiczenie2
+{
+    public class PlikWyjsciowy
+    {
+
+
+
+
+
+    }
+}
